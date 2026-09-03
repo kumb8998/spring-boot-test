080648a0a122a5106b9048d882efd239ca23318a
kumb8998 <aniket.kumbhar8998.com>
2026-09-03 14:23:49 +0530

    todays new branch with changes

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Main Class");
-        System.out.println("Directly from browser");
+        System.out.println("Welcome today");
     }
 }
